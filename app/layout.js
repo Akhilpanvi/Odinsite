6bbbe64d92fa8456131f73eab74917bc7fce2093
AKHIL PANVI CHAKKAPALLI <2300033181@kluniversity.in>
2025-11-03 00:42:25 +0530

    Add viewport meta tag to RootLayout

--- app/layout.js
+++ app/layout.js
@@ -1,10 +1,16 @@
 export const metadata = {
-  title: "Video Background"
+  title: "Video Background",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0"
+        />
+      </head>
       <body style={{ margin: 0 }}>{children}</body>
     </html>
   );
